3686607b93c181853ff33c79783afc2b56cd21d5
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-14 23:56:15 +0200

    Auth0 button update

--- components/shared/AuthButton/index.tsx
+++ components/shared/AuthButton/index.tsx
@@ -19,9 +19,15 @@ const AuthButton: FunctionComponent = () => {
 				<Link href="/api/auth/logout">
 					<a className="rounded-sm px-3 shadow-sm">Log Out</a>
 				</Link>
-				<div className="">
-					<img src={user?.picture} alt="user profile" className="rounded-full h-10" />
-				</div>
+				{/* <div className="">
+					<img
+						// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+						// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+						src={user?.profile!}
+						alt="user profile"
+						className="rounded-full h-10"
+					/>
+				</div> */}
 			</>
 		);
 	}
