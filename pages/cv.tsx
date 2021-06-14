1424962dfa5be0b03184bdbdb87d85bd6bd9f5ea
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-14 23:06:20 +0200

    cv page

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,38 +1,21 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable camelcase */
 import Head from "next/head";
-import { useUser, UserProfile, withPageAuthRequired } from "@auth0/nextjs-auth0";
+// components
+import BannerLayout from "components/layout/BannerLayout";
 
-type ProfileProps = { user: UserProfile };
-
-const CV: React.FunctionComponent<ProfileProps> = ({ user }) => {
-  const { error, isLoading } = useUser();
-
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
-
-  return (
-    <>
-      <Head>
-        <title>Curriculum Vitae</title>
-      </Head>
-      <div className="flex flex-col items-center h-full">
-        <h1 className="text-center text-8xl w-full">Curriculum Vitae Page</h1>
-        {user && (
-          <div>
-            <h1>Welcome, {user?.nickname} has been granted access to my Curriculum Vitae.</h1>
-            <p>Curriculum Vitae Component</p>
-          </div>
-        )}
-        {!user && <div>Please sign up to request CV for security reasons</div>}
-        {error && <div>An error has occurred, please sign in to continue</div>}
-        <pre data-testid="profile">{JSON.stringify(user, null, 2)}</pre>
-      </div>
-    </>
-  );
-};
+const CV: React.FunctionComponent = () => (
+	<>
+		<Head>
+			<title>Curriculum Vitae</title>
+		</Head>
+		<BannerLayout
+			summary="For security reasons, you have request access to this file by filling the form below."
+			title="Curriculum Vitae"
+			bgColor="bg-orange-600"
+			color="text-gray-50"
+		>
+			<p>Request access</p>
+		</BannerLayout>
+	</>
+);
 
 export default CV;
-
-export const getServerSideProps = withPageAuthRequired();
