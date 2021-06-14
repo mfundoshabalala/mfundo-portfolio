3a11c6490fbffbb428a35a86bd079e27ab9a01b2
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-14 23:06:55 +0200

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,31 @@ import Head from "next/head";
 import Image from "next/image";
 //
 import TypedText from "components/shared/TypedText";
+import { NextPage } from "next";
 
 // TODO: Add a catchy background song on full page load
-const Home = () => (
-  <>
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-      <title>Home</title>
-    </Head>
-    <div className="h-full font-body filter drop-shadow-md">
-      <div className="h-full flex flex-row-reverse justify-evenly">
-        <div className="relative flex-1">
-          <Image
-            className="object-contain object-bottom"
-            src="/images/programmer-animate.svg"
-            alt="Picture of the author"
-            layout="fill"
-          />
-        </div>
-        <div className="relative flex-1 flex flex-col">
-          <TypedText />
-        </div>
-      </div>
-    </div>
-  </>
+const Home: NextPage = () => (
+	<>
+		<Head>
+			<link rel="icon" href="/favicon.ico" />
+			<title>Home</title>
+		</Head>
+		<div className="h-full font-body filter drop-shadow-md">
+			<div className="h-full flex flex-row-reverse justify-evenly">
+				<div className="relative flex-1">
+					<Image
+						className="object-contain object-bottom"
+						src="/images/programmer-animate.svg"
+						alt="Picture of the author"
+						layout="fill"
+					/>
+				</div>
+				<div className="relative flex-1 flex flex-col">
+					<TypedText />
+				</div>
+			</div>
+		</div>
+	</>
 );
 
 export default Home;
