afafb9a5529e21ed0ea6f7e5c8396710a04e8bb6
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-16 15:42:33 +0200

    Update tailwind.config.js purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	purge: {
+    	enabled: process.env.NODE_ENV === "production",
+    	content: [
+      		"./{components,pages}/**/*.{js,ts,jsx,tsx}"
+    		],
+  	},
 	darkMode: "class", // or 'media' or 'class'
 	theme: {
 		colors: {
