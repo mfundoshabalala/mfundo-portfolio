cfd832a752f57d660094c13a3ebbe657c8e65949
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-16 18:19:11 +0200

    Header and components update

--- components/shared/Header/index.tsx
+++ components/shared/Header/index.tsx
@@ -5,7 +5,7 @@ import BrandLogo from "../BrandLogo";
 import NavForm from "../NavForm";
 
 const Header: FunctionComponent = () => (
-	<header className="bg-gray-100 text-center font-body font-normal tracking-tight sticky top-0 left-0 flex flex-row justify-between items-center h-16 px-8 py-6">
+	<header className="z-30 sticky top-0 left-0 border border-solid border-gray-200 bg-gray-100 text-center font-body font-normal tracking-tight flex flex-row justify-between items-center h-16 px-8 py-6">
 		<BrandLogo />
 		<Navbar />
 		<NavForm />
--- components/shared/Navbar/index.tsx
+++ components/shared/Navbar/index.tsx
@@ -7,10 +7,10 @@ const Navbar: FunctionComponent = () => (
 		<nav className="leading-9 flex-1 flex flex-row justify-center">
 			<ul className="flex flex-row justify-center space-x-3">
 				<NavLink name="About" url="/about" />
-				<NavLink name="Portfolio" url="/portfolio" bgColor="blue-500" />
-				<NavLink name="Blog" url="/blog" bgColor="green-500" />
-				<NavLink name="Resume" url="/cv" bgColor="yellow-500" />
-				<NavLink name="Contact" url="/contact" bgColor="gray-500" />
+				<NavLink name="Portfolio" url="/portfolio" />
+				<NavLink name="Blog" url="/blog" />
+				<NavLink name="Resume" url="/cv" />
+				<NavLink name="Contact" url="/contact" />
 			</ul>
 		</nav>
 	</>
