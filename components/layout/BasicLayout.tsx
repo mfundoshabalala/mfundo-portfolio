5a4e282169bc270e6cd10ee79b0d2a0723749354
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-16 18:14:10 +0200

    Basic Layout background wave update

--- components/layout/BasicLayout.tsx
+++ components/layout/BasicLayout.tsx
@@ -1,17 +1,21 @@
-import { NextPage } from "next";
 import PropTypes from "prop-types";
+import { FunctionComponent } from "react";
 // page component(s)
 import Header from "components/shared/Header";
 import Footer from "components/shared/Footer";
-// page props interface(s)
-// import BackgroundAnimatedIcon from "components/shared/BackgroundAnimationIcon";
 
-const BasicLayout: NextPage<IBasicLayout> = ({ children }) => (
-	<div className="h-full flex flex-col relative">
+export const BackgroundWave: FunctionComponent = () => (
+	<>
+		<div className="w-full h-1/3 bg-wave bg-no-repeat bg-cover bg-center z-0 absolute bottom-0 left-0" />
+	</>
+);
+
+const BasicLayout: FunctionComponent<IBasicLayout> = ({ children }) => (
+	<div className="h-full flex flex-col relative text-gray-600 overflow-x-hidden">
 		<Header />
-		<main className="flex-1">{children}</main>
+		<main className="flex-1 relative z-10">{children}</main>
 		<Footer />
-		{/* <BackgroundAnimatedIcon   /> */}
+		<BackgroundWave />
 	</div>
 );
 
