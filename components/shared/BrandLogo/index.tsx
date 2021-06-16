e31b1bde0327ea696677356922278b09800abaa1
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-16 16:02:33 +0200

    BrandLogo update

--- components/shared/BrandLogo/index.tsx
+++ components/shared/BrandLogo/index.tsx
@@ -2,15 +2,11 @@ import Link from "next/link";
 import { FunctionComponent } from "react";
 
 const BrandLogo: FunctionComponent = () => (
-	<div className="font-black text-5xl">
+	<div className="font-black text-5xl px-1 py-2">
 		<Link href="/">
-			<a className="block">
-				<span className="">M</span>
-				<span className="">f</span>
-				<span className="">u</span>
-				<span className="">n</span>
-				<span className="">d</span>
-				<span className="">o</span>
+			<a className="block text-gray-50">
+				<span className="bg-blue-500 border-2 border-solid border-green-500">M</span>
+				<span className="bg-green-500 border-2 border-solid border-blue-500">F</span>
 			</a>
 		</Link>
 	</div>
