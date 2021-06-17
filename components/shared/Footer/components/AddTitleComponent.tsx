e87fb645803920a1d22c9cf85474b2bc79f85d5b
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2021-06-17 21:10:15 +0200

    adding shadows on the title

--- components/shared/Footer/components/AddTitleComponent.tsx
+++ components/shared/Footer/components/AddTitleComponent.tsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const AddTitleComponent: FunctionComponent<IAddTitle> = ({ children, title }) => (
 	<>
 		<div className="flex-1 flex flex-col justify-center items-center space-y-4">
-			<h1 className="capitalize font-body font-semibold text-xl">{title}</h1>
+			<h1 className="text-shadow capitalize font-body font-semibold text-xl">{title}</h1>
 			<>{children}</>
 		</div>
 	</>
