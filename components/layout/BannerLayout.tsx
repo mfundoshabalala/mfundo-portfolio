ca9705182205976032f1e9cd8887b4e767efc884
Mfundo <mfundoshabalala@gmail.com>
2021-07-06 05:48:25 +0200

    banner layout component update

--- components/layout/BannerLayout.tsx
+++ components/layout/BannerLayout.tsx
@@ -1,46 +1,19 @@
-import { FunctionComponent } from 'react';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 import PropTypes from 'prop-types';
-// components
+
 import Banner from 'components/shared/Banner';
 
-const bannerVariant = {
-	hidden: { x: '100vw', opacity: 0 },
-	show: { x: 0, opacity: 1, transition: { staggerChildren: 2, type: 'spring', stiffness: 50 } },
-	exit: { x: '-100vw', opacity: 0, transition: { duration: 10 } },
-};
-
-const BannerLayout: FunctionComponent<IBannerLayout> = ({ children, ...rest }) => (
-	<motion.div
-		variants={bannerVariant}
-		initial="hidden"
-		animate="show"
-		exit="exit"
+const BannerLayout: React.FC<IBannerLayout> = ({ children, ...rest }) => (
+	<div
 		className="flex flex-col w-full min-h-full"
 	>
 		<Banner {...rest} />
 		{children}
-	</motion.div>
+	</div>
 );
 
 export default BannerLayout;
 
 BannerLayout.propTypes = {
 	children: PropTypes.node.isRequired,
-};
-
-BannerLayout.propTypes = {
-	color: PropTypes.string,
-	bgColor: PropTypes.string,
-	title: PropTypes.string,
-	summary: PropTypes.string,
-	quote: PropTypes.string,
-};
-
-BannerLayout.defaultProps = {
-	color: 'text-gray-50',
-	bgColor: 'bg-blue-600',
-	title: '',
-	summary: '',
-	quote: undefined,
 };
