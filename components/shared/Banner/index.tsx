aea4d8c901e37fc27b0755aac288c86dc1e6ced0
Mfundo <mfundoshabalala@gmail.com>
2021-07-08 03:32:29 +0200

    banner component update

--- components/shared/Banner/index.tsx
+++ components/shared/Banner/index.tsx
@@ -1,29 +1,26 @@
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import { FunctionComponent } from 'react';
+import classNames from 'classnames';
 
-const Banner: FunctionComponent<IBanner> = ({ color, bgColor, title, summary }) => (
-	<>
-		<div className={classnames(bgColor, 'py-14 shadow-custom text-shadow')}>
-			<div className={classnames(color, 'space-y-2 h-full container flex flex-col justify-center items-center')}>
-				<h1 className="text-5xl font-body font-bold">{title}</h1>
+const Banner: React.FC<IBanner> = ({ bgColor, title, summary }) => (
+	<div className="text-white bg-gray-900 rellax py-14 shadow-custom">
+		<div className="container flex flex-col items-center justify-center h-full space-y-2 text-center">
+			<div className={classNames(bgColor, 'bg-gradient-to-r bg-clip-text')}>
+				<h1 className="text-5xl font-bold text-transparent font-body">{title}</h1>
 				<h2 className="text-lg">{summary}</h2>
 			</div>
 		</div>
-	</>
+	</div>
 );
 
 export default Banner;
 
 Banner.propTypes = {
-	color: PropTypes.string,
 	bgColor: PropTypes.string,
 	title: PropTypes.string,
 	summary: PropTypes.string,
 };
 
 Banner.defaultProps = {
-	color: 'text-gray-50',
 	bgColor: 'bg-blue-600',
 	title: '',
 	summary: '',
