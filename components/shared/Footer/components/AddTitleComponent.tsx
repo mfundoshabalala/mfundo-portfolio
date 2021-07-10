8af57ef3b5eafc5aa330d65c3186476079602bdd
Mfundo <mfundoshabalala@gmail.com>
2021-07-10 07:34:47 +0200

    footer subcomponent update

--- components/shared/Footer/components/AddTitleComponent.tsx
+++ components/shared/Footer/components/AddTitleComponent.tsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const AddTitleComponent: React.FC<IAddTitle> = ({ children, title }) => (
 	<>
-		<div className="flex flex-col items-center justify-center flex-1 space-y-2 md:space-y-4">
+		<div className="flex flex-col items-center justify-between flex-1 space-y-2 md:space-y-4">
 			<h1 className="text-xl font-thin capitalize text-shadow">{title}</h1>
 			<>{children}</>
 		</div>
