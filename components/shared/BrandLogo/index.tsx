54aed63f0d4bec44845a72c9ac9d1f25595d78b1
Mfundo <mfundoshabalala@gmail.com>
2021-07-10 06:40:04 +0200

    bramd logo component update

--- components/shared/BrandLogo/index.tsx
+++ components/shared/BrandLogo/index.tsx
@@ -1,15 +1,10 @@
 import Link from 'next/link';
 
 const BrandLogo: React.FC = () => (
-	<div className="px-1 py-2 text-4xl font-black text-transparent bg-gradient-to-r bg-clip-text from-gray-900 via-gray-600 to-gray-900">
+	<div className="px-1 py-2 text-4xl font-black text-transparent min-w-max font-firaSans bg-gradient-to-r bg-clip-text from-gray-900 via-gray-800 to-gray-700">
 		<Link href="/">
-			<a className="block tracking-tighter uppercase">
-				<span className="inline-block">M</span>
-				<span className="inline-block">f</span>
-				<span className="inline-block">u</span>
-				<span className="inline-block">n</span>
-				<span className="inline-block">d</span>
-				<span className="inline-block">o</span>
+			<a className="block tracking-tighter hover:line-through min-w-max">
+				Mfundo<sub className="bg-gradient-to-r bg-clip-text from-gray-900 via-gray-800 to-gray-700">92</sub>
 			</a>
 		</Link>
 	</div>
