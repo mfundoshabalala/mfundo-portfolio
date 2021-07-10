fb37bed793f6b27eb86d8c9a41df0c7e38775720
Mfundo <mfundoshabalala@gmail.com>
2021-07-10 06:39:45 +0200

    banner component update

--- components/shared/Banner/index.tsx
+++ components/shared/Banner/index.tsx
@@ -3,10 +3,10 @@ import classNames from 'classnames';
 
 const Banner: React.FC<IBanner> = ({ bgColor, title, summary }) => (
 	<div className="text-white bg-gray-900 rellax py-14 shadow-custom">
-		<div className="container flex flex-col items-center justify-center h-full space-y-2 text-center">
-			<div className={classNames(bgColor, 'bg-gradient-to-r bg-clip-text')}>
-				<h1 className="text-5xl font-bold text-transparent font-body">{title}</h1>
-				<h2 className="text-lg">{summary}</h2>
+		<div className="container flex flex-col items-center justify-center h-full mx-auto tracking-wide text-center">
+			<div className={classNames(bgColor, 'bg-gradient-to-r bg-clip-text space-y-2')}>
+				<h1 className="text-5xl font-extrabold text-transparent font-firaSans">{title}</h1>
+				<h2 className="text-lg font-light opacity-80">{summary}</h2>
 			</div>
 		</div>
 	</div>
