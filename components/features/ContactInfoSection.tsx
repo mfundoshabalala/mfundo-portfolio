a4f110e3fa6bb2259c0f49af286deaf61e448537
Mfundo <mfundoshabalala@gmail.com>
2021-07-12 15:50:47 +0200

    contact page feature update

--- components/features/ContactInfoSection.tsx
+++ components/features/ContactInfoSection.tsx
@@ -1,17 +1,17 @@
 import IconComponent from 'components/shared/IconComponent';
 
 const ContactInfoSection: React.FC = () => (
-	<section className="flex flex-col justify-center mx-auto space-y-2 sm:space-x-6 sm:flex-row sm:space-y-0 min-w-max">
-		<span className="">
+	<section className="flex flex-col justify-between max-w-xl mx-auto space-y-2 sm:space-x-6 sm:flex-row sm:space-y-0 min-w-max">
+		<span className="flex flex-row items-center justify-center space-x-2 flex-nowrap">
 			<IconComponent icon="email" />
-			<a href="" className="hover:underline">
+			<a href="" className="hover:underline hover:text-blue-700">
 				mfundoshabalala@gmail.com
 			</a>
 		</span>
-		<span className="">
+		<span className="flex flex-row items-center justify-center space-x-2 flex-nowrap">
 			<IconComponent icon="phone" />
-			<a href="" className="hover:underline">
-				+27 604 843 666
+			<a href="" className="hover:underline hover:text-blue-700">
+				060 484 3666
 			</a>
 		</span>
 	</section>
