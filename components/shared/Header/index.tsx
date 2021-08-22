d3daa8cea042e35efab8416a126d35684d0b711c
Mfundo <mfundoshabalala@gmail.com>
2021-08-22 20:20:17 +0200

    header update

--- components/shared/Header/index.tsx
+++ components/shared/Header/index.tsx
@@ -7,10 +7,10 @@ const Header: React.FC = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<header className="sticky top-0 left-0 z-30 flex flex-row items-center justify-between h-16 px-6 py-6 font-normal tracking-tight text-center bg-gray-100 border border-gray-200 border-solid md:px-6 font-body">
+		<header className="sticky top-0 left-0 z-30 flex flex-row items-center justify-between h-16 px-6 py-6 font-normal tracking-tight text-center bg-gray-100 border border-gray-200 border-solid md:px-6">
 			<BrandLogo />
 			<Navbar isOpen={isOpen} />
-			<NavMenu isOpen={isOpen} onClick={() => setIsOpen(!isOpen) } />
+			<NavMenu isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
 		</header>
 	);
 };
