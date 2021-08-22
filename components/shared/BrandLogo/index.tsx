5ec8eb8e9dea0e81ed67da0c9165d8038289792f
Mfundo <mfundoshabalala@gmail.com>
2021-08-22 20:15:46 +0200

    BrandLogo update

--- components/shared/BrandLogo/index.tsx
+++ components/shared/BrandLogo/index.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 
 const BrandLogo: React.FC = () => (
-	<div className="px-1 py-2 text-[36px] tracking-tighter font-black text-transparent min-w-max font-firaSans bg-gradient-to-r bg-clip-text from-gray-700 to-gray-600">
+	<div className="px-1 py-2 text-2xl font-black tracking-tighter text-transparent min-w-max font-firaSans bg-gradient-to-r bg-clip-text from-gray-700 to-gray-600">
 		<Link href="/">
 			<a className="block mx-2 uppercase hover:line-through min-w-max">
-				MFUNDO<sub className="pr-1 mr-1 bg-gradient-to-r bg-clip-text from-gray-700 to-gray-600">92</sub>
+				Mfundo Shabalala
 			</a>
 		</Link>
 	</div>
