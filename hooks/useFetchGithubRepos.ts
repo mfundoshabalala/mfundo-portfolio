02ffed1c60258c4918ede96cd2f894d0a93a4292
Mfundo <mfundoshabalala@gmail.com>
2021-08-24 00:52:38 +0200

    fetch github data hook

---
+++ hooks/useFetchGithubRepos.ts
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import useSWR from 'swr';
+
+const useFetchGithubRepos = (): GithubRepo[] | undefined => {
+	const { data: githubRepos } = useSWR<GithubRepo[] | undefined>(
+		process.env.NEXT_PUBLIC_GITHUB_USERNAME
+			? `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USERNAME || ''}/repos`
+			: null
+	);
+
+	return githubRepos;
+};
+
+export default useFetchGithubRepos;
