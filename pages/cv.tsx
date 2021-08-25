062088736bb2d74d20533e37de572f18664f2542
Mfundo <mfundoshabalala@gmail.com>
2021-08-25 13:34:15 +0200

    pages update

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,37 +1,76 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { NextPage } from 'next';
+import _ from 'lodash';
 // components
 import BannerLayout from 'components/layout/BannerLayout';
 
-const CV: NextPage = () => (
-	<>
-		<Head>
-			<title>Curriculum Vitae</title>
-		</Head>
-		<BannerLayout
-			title="Curriculum Vitae"
-			bgColor="from-red-800 via-red-500 via-red-500 to-red-800"
-			summary="For security reasons, you have request access to this file by filling the form below."
-		>
-			<div className="flex flex-col items-center space-y-10">
-				<section className="flex justify-between md:flex-row md:space-x-4">
-					<a href="" className="relative w-10 h-10 rounded">
-						<Image
-							src="/pdf/CERTIFICATE_LANDING_PAGE_3NDHZJ96EDDN.jpeg"
-							layout="fill"
-							alt="Convolutional Neural Networks Certification"
-						/>
-					</a>
-				</section>
-				<div className="grid place-items">
-					<button type="button" className="btn btn-primary">
-						Request Full CV
-					</button>
+const CV: NextPage = () => {
+	const pageDetails = {
+		title: 'Curriculum Vitae',
+		bgColor: 'from-red-800 via-red-500 via-red-500 to-red-800',
+		summary: 'For security reasons, you have request access to this file by filling the form below.',
+	};
+
+	const certifications = [
+		{
+			title: 'Certifications 1',
+			url: '/pdf/Coursera 3NDHZJ96EDDN.pdf',
+		},
+		{
+			title: 'Certifications 2',
+			url: '/pdf/Coursera AQLUW5TMBVU7.pdf',
+		},
+		{
+			title: 'Certifications 3',
+			url: '/pdf/Coursera FKAX92999XNE.pdf',
+		},
+		{
+			title: 'Certifications 4',
+			url: '/pdf/Coursera N6BZPEHYWZNE.pdf',
+		},
+		{
+			title: 'Certifications 5',
+			url: '/pdf/Coursera PQ8N2QGF8XXC.pdf',
+		},
+		{
+			title: 'Certifications 6',
+			url: '/pdf/Coursera SCZZHUF23QDH.pdf',
+		},
+		{
+			title: 'Certifications 7',
+			url: '/pdf/Coursera Z7MHS723E35B.pdf',
+		},
+	];
+
+	return (
+		<>
+			<Head>
+				<title>Curriculum Vitae</title>
+			</Head>
+			<BannerLayout {...pageDetails}>
+				<div className="flex flex-col items-center py-10 gap-y-10">
+					<section className="container flex flex-wrap w-full gap-6 mx-auto">
+						{_.map(certifications, ({ url, title }: { url: string; title: string }) => (
+							<iframe
+								title={title}
+								src={url}
+								width="auto"
+								height={400}
+								frameBorder="0"
+								className="pointer-events-none"
+							/>
+						))}
+					</section>
+					<div className="grid place-items">
+						<button type="button" className="btn btn-primary">
+							Request Full CV
+						</button>
+					</div>
 				</div>
-			</div>
-		</BannerLayout>
-	</>
-);
+			</BannerLayout>
+		</>
+	);
+};
 
 export default CV;
