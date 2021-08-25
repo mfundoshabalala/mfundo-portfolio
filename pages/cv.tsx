a70bdb6a34a8455727e976e9484c994ffeffe23f
Mfundo <mfundoshabalala@gmail.com>
2021-08-25 16:09:14 +0200

    resume page refactoring

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { NextPage } from 'next';
-import _ from 'lodash';
 // components
 import BannerLayout from 'components/layout/BannerLayout';
+import CertificatesList from 'components/features/CertificatesList';
 
 const CV: NextPage = () => {
 	const pageDetails = {
@@ -12,37 +11,6 @@ const CV: NextPage = () => {
 		summary: 'For security reasons, you have request access to this file by filling the form below.',
 	};
 
-	const certifications = [
-		{
-			title: 'Certifications 1',
-			url: '/pdf/Coursera 3NDHZJ96EDDN.pdf',
-		},
-		{
-			title: 'Certifications 2',
-			url: '/pdf/Coursera AQLUW5TMBVU7.pdf',
-		},
-		{
-			title: 'Certifications 3',
-			url: '/pdf/Coursera FKAX92999XNE.pdf',
-		},
-		{
-			title: 'Certifications 4',
-			url: '/pdf/Coursera N6BZPEHYWZNE.pdf',
-		},
-		{
-			title: 'Certifications 5',
-			url: '/pdf/Coursera PQ8N2QGF8XXC.pdf',
-		},
-		{
-			title: 'Certifications 6',
-			url: '/pdf/Coursera SCZZHUF23QDH.pdf',
-		},
-		{
-			title: 'Certifications 7',
-			url: '/pdf/Coursera Z7MHS723E35B.pdf',
-		},
-	];
-
 	return (
 		<>
 			<Head>
@@ -50,23 +18,10 @@ const CV: NextPage = () => {
 			</Head>
 			<BannerLayout {...pageDetails}>
 				<div className="flex flex-col items-center py-10 gap-y-10">
-					<section className="container flex flex-wrap w-full gap-6 mx-auto">
-						{_.map(certifications, ({ url, title }: { url: string; title: string }) => (
-							<iframe
-								title={title}
-								src={url}
-								width="auto"
-								height={400}
-								frameBorder="0"
-								className="pointer-events-none"
-							/>
-						))}
-					</section>
-					<div className="grid place-items">
-						<button type="button" className="btn btn-primary">
-							Request Full CV
-						</button>
-					</div>
+					<CertificatesList />
+					<button type="button" className="btn btn-primary">
+						Request Full CV
+					</button>
 				</div>
 			</BannerLayout>
 		</>
