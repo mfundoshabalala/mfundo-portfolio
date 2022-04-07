dbc7df443a1fc4f4653c1aee381a48b16d998bb6
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-07 09:26:23 +0200

    index.tsx page update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,21 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
-import IntroductionImage from 'components/features/IntroductionImage';
-import IntroductionDescription from 'components/features/IntroductionDescription';
+// Pages Section Components
+import HomeSection from 'components/pages/HomeSection';
+import AboutSection from 'components/pages/AboutSection';
+import ContactSection from 'components/pages/ContactSection';
+import ProjectsSection from 'components/pages/ProjectsSection';
 
 const Home: NextPage = () => (
 	<>
 		<Head>
 			<link rel="icon" href="/favicon.ico" />
-			<title>Home</title>
+			<title>Mfundo Shabalala Portfolio</title>
 		</Head>
-		<section className="container h-full mx-auto">
-			<div className="flex flex-col h-full md:flex-row justify-evenly">
-				<IntroductionImage />
-				<IntroductionDescription />
-			</div>
-		</section>
+		<HomeSection />
+		<AboutSection />
+		<ProjectsSection />
+		<ContactSection />
 	</>
 );
 
