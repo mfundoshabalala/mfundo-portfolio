9a615dab983999924ecd932a982930bdad123180
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-07 09:25:49 +0200

    _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -6,7 +7,7 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head />
-				<body className="text-gray-900 bg-gray-100">
+				<body className="text-gray-900 bg-gray-100 overflow-x-hidden">
 					<Main />
 					<NextScript />
 				</body>
