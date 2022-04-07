1ab405747e4094a7044a97b97ab989af01674f5b
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-07 09:27:10 +0200

    fontawesome utilility

---
+++ lib/fontawesome.ts
@@ -0,0 +1,7 @@
+// import the library
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import your icons
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+// add the icons to the library
+library.add(fab, fas);
