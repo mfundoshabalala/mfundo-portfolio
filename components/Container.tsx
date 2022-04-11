8b2b4a5286e4d6e64a7df9b948dc0ec54f168ab6
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-11 20:18:20 +0200

    container component

---
+++ components/Container.tsx
@@ -0,0 +1,12 @@
+interface ContainerInterface {
+	children: React.ReactNode;
+	className?: string | undefined;
+}
+
+type ContainerProps = React.FunctionComponent<ContainerInterface>;
+
+const Container: ContainerProps = ({ children, className }) => (
+	<section className={`container mx-auto ${className ?? ''}`}>{children}</section>
+);
+
+export default Container;
