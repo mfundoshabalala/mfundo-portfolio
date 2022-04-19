90de95e8161cad373e1b3cf2f1a67a57384d7227
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-19 17:43:26 +0200

    site layout update

--- components/layout/BasicSiteLayout.tsx
+++ components/layout/BasicSiteLayout.tsx
@@ -1,10 +1,8 @@
-//
-import Header from 'components/Header';
 import TopNav from 'components/TopNav';
 import Navbar from 'components/Navbar';
-import IconComponent from 'components/IconComponent';
-import FixedPositionPanel from 'components/FixedPositionPanel';
-//
+import Header from 'components/shared/Header';
+import IconComponent from 'components/shared/IconComponent';
+import FixedPositionPanel from 'components/shared/FixedPositionPanel';
 
 type BasicSiteLayoutProps = React.FunctionComponent<{
 	children: React.ReactChild;
@@ -12,19 +10,34 @@ type BasicSiteLayoutProps = React.FunctionComponent<{
 
 const BasicSiteLayout: BasicSiteLayoutProps = ({ children }) => (
 	<>
-		<Header className="flex flex-col dark-section">
+		<Header className="flex flex-col bg-slate-800">
 			<TopNav />
 			<Navbar />
 		</Header>
 
 		<>{children}</>
-		<FixedPositionPanel className="bottom-20 left-2 py-4 px-3 flex flex-col justify-center gap-y-2">
-			<IconComponent icon={['fab', 'github']} href="https://github.com/shbmfu003" />
-			<IconComponent icon={['fab', 'linkedin-in']} href="https://www.linkedin.com/in/mfundoshabalala" />
-			<IconComponent icon={['fab', 'twitter']} href="https://www.twitter.com/datarist" />
+
+		<FixedPositionPanel className="bottom-20 left-4 py-4 px-3 flex flex-col justify-center gap-y-2">
+			<IconComponent
+				icon={['fab', 'github']}
+				href={process.env.NEXT_PUBLIC_GITHUB_USER_PROFILE}
+			/>
+			<IconComponent
+				icon={['fab', 'linkedin-in']}
+				href={process.env.NEXT_PUBLIC_LINKEDIN_USER_PROFILE}
+			/>
+			<IconComponent
+				icon={['fab', 'twitter']}
+				href={process.env.NEXT_PUBLIC_TWITTER_USER_PROFILE}
+			/>
 		</FixedPositionPanel>
-		<FixedPositionPanel className="bottom-20 mb-12 right-2 flex items-center p-3">
-			<IconComponent bounce size="lg" icon={['fas', 'cloud-arrow-down']} href={process.env.NEXT_PUBLIC_CV_URL} />
+		<FixedPositionPanel className="bottom-10 mb-12 right-4 flex items-center p-3">
+			<IconComponent
+				bounce
+				size="lg"
+				icon={['fas', 'cloud-arrow-down']}
+				href={process.env.NEXT_PUBLIC_CV_URL}
+			/>
 		</FixedPositionPanel>
 	</>
 );
