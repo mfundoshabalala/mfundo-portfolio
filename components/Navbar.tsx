0bfc294a5d603f1f033ae927abb52741eded0d65
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-20 22:38:58 +0200

    Navbar.tsx update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
-import NavItem from './NavItem';
-import BrandLogo from './BrandLogo';
-import NavbarList from './NavbarList';
+import NavItem from './shared/NavItem';
+import BrandLogo from './shared/BrandLogo';
+import NavbarList from './shared/NavbarList';
 
 const Navbar: React.FunctionComponent = () => (
 	<nav className="w-full text-gray-100 h-16 flex items-center opacity-100 shadow-sm bg-slate-800 border-t-2 border-slate-700">
