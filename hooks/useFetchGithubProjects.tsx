80d5c60a2ee95c7ebd459b61c8a951439f813ad7
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-20 22:43:07 +0200

    fetch github projects hook

--- hooks/useFetchGithubProjects.tsx
+++ hooks/useFetchGithubProjects.tsx
@@ -1,7 +1,7 @@
 import useSWR from 'swr';
 
-const useFetchGithubProjects = (): GithubRepositories[] | undefined => {
-	const { data: githubProjects } = useSWR<GithubRepositories[] | undefined>(
+const useFetchGithubProjects = (): GithubRepository[] | undefined => {
+	const { data: githubProjects } = useSWR<GithubRepository[] | undefined>(
 		process.env.NEXT_PUBLIC_GITHUB_USERNAME
 			? `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USERNAME}/repos`
 			: null
