da4ab7db5f272c62b00c73986b037760b8bfde70
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-20 22:40:07 +0200

    basic site layout update

--- components/layout/BasicSiteLayout.tsx
+++ components/layout/BasicSiteLayout.tsx
@@ -1,8 +1,7 @@
 import TopNav from 'components/TopNav';
 import Navbar from 'components/Navbar';
 import Header from 'components/shared/Header';
-import IconComponent from 'components/shared/IconComponent';
-import FixedPositionPanel from 'components/shared/FixedPositionPanel';
+import ResourcesPanel from 'components/ResourcesPanel';
 
 type BasicSiteLayoutProps = React.FunctionComponent<{
 	children: React.ReactChild;
@@ -10,35 +9,13 @@ type BasicSiteLayoutProps = React.FunctionComponent<{
 
 const BasicSiteLayout: BasicSiteLayoutProps = ({ children }) => (
 	<>
-		<Header className="flex flex-col bg-slate-800">
+		<Header className="flex flex-col bg-slate-800 w-full fixed top-0 left-0 z-50">
 			<TopNav />
 			<Navbar />
 		</Header>
-
-		<>{children}</>
-
-		<FixedPositionPanel className="bottom-20 left-4 py-4 px-3 flex flex-col justify-center gap-y-2">
-			<IconComponent
-				icon={['fab', 'github']}
-				href={process.env.NEXT_PUBLIC_GITHUB_USER_PROFILE}
-			/>
-			<IconComponent
-				icon={['fab', 'linkedin-in']}
-				href={process.env.NEXT_PUBLIC_LINKEDIN_USER_PROFILE}
-			/>
-			<IconComponent
-				icon={['fab', 'twitter']}
-				href={process.env.NEXT_PUBLIC_TWITTER_USER_PROFILE}
-			/>
-		</FixedPositionPanel>
-		<FixedPositionPanel className="bottom-10 mb-12 right-4 flex items-center p-3">
-			<IconComponent
-				bounce
-				size="lg"
-				icon={['fas', 'cloud-arrow-down']}
-				href={process.env.NEXT_PUBLIC_CV_URL}
-			/>
-		</FixedPositionPanel>
+		<main className="relative z-40">{children}</main>
+		{/* <footer>Footer Component</footer> */}
+		<ResourcesPanel />
 	</>
 );
 
