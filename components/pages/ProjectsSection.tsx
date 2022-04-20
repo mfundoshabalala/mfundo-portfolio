83637e534fc13731ad2acab2f16a1e7d7093ba9a
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-20 22:37:58 +0200

    project page section update

--- components/pages/ProjectsSection.tsx
+++ components/pages/ProjectsSection.tsx
@@ -1,14 +1,39 @@
-/* eslint-disable react/no-array-index-key */
-import PageSection from 'components/PageSection';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import _ from 'lodash';
+import { useEffect, useState } from 'react';
+//
+import PageSection from 'components/shared/PageSection';
 import ProjectListItem from 'components/ProjectListItem';
+import filterGithubRepository from 'lib/filterGithubProjects';
+import useFetchGithubProjects from 'hooks/useFetchGithubProjects';
+import BackgroundPattern from 'components/shared/BackgroundPattern';
 
-const ProjectsSection: React.FunctionComponent = () => (
-	<PageSection pageID="projects" className={['', 'flex-wrap gap-5 py-16']}>
-		<h1 className="w-full text-center text-5xl font-black uppercase tracking-wide my-16">My Work</h1>
-		{[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item, i) => (
-			<ProjectListItem key={i} />
-		))}
-	</PageSection>
-);
+const ProjectsSection: React.FunctionComponent = () => {
+	const [projects, setProjects] = useState<GithubRepository[] | undefined>(
+		undefined
+	);
+	const githubProjects = useFetchGithubProjects();
+	const filteredGithubProjects = filterGithubRepository(githubProjects);
+
+	useEffect(() => {
+		if (!_.isEqual(projects, [])) return;
+		setProjects(filteredGithubProjects);
+	}, [filteredGithubProjects, projects]);
+
+	return (
+		<PageSection pageID="projects" className={['', 'flex-wrap gap-4']}>
+			<h1 className="w-full text-center text-5xl font-black uppercase tracking-wide py-4">
+				<div className="bg-clip-text bg-gradient-to-r from-orange-800 via-orange-500  to-orange-800">
+					<span className="text-transparent">My Work</span>
+				</div>
+			</h1>
+			{_.map(projects ?? githubProjects, (project: GithubRepository) => (
+				<ProjectListItem key={project.id} project={project} />
+			))}
+			<BackgroundPattern className="bg-cogs-pattern" />
+		</PageSection>
+	);
+};
 
 export default ProjectsSection;
