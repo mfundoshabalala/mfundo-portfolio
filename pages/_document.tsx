6df7dae88df5496022f175ee5053663448a88c39
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-20 22:43:40 +0200

    _document.tsx update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,13 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // eslint-disable-next-line @next/next/no-document-import-in-page
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-	render() {
+	render(): JSX.Element {
 		return (
 			<Html lang="en">
-				<Head />
+				<Head>
+					<meta httpEquiv="Content-Type" content="text/html;charset=utf-8" />
+				</Head>
 				<body className="text-gray-900 bg-gray-100 overflow-x-hidden">
 					<Main />
 					<NextScript />
