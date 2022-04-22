8bd10a58778d837d34c6ee7a38dc03364984f0df
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-22 05:26:03 +0200

    block quote component

--- components/shared/BlockQuote.tsx
+++ components/shared/BlockQuote.tsx
@@ -2,18 +2,24 @@ type BlockQuoteProps = React.FunctionComponent<{
 	children: React.ReactNode;
 	className?: string | undefined;
 	cite?: string;
+	author?: string;
 }>;
 
-const BlockQuote: BlockQuoteProps = ({ children, cite, className }) => (
-	<blockquote
-		cite={cite}
-		className={`max-w-full lg:max-w-md mx-auto capitalize
-			text-justify font-thin opacity-50 font-mono ${className ?? ''}`}
+const BlockQuote: BlockQuoteProps = ({ children, cite, className, author }) => (
+	<figure
+		className={`max-w-2xl capitalize text-center font-thin opacity-50 font-mono italic py-4 rounded ${
+			className ?? ''
+		}`}
 	>
-		<span>&ldquo;</span>
-		{children}
-		<span>&ldquo;</span>
-	</blockquote>
+		<blockquote cite={cite}>
+			<>
+				<span className="pr-1">&ldquo;</span>
+				{children}
+				<span>&rdquo;</span>
+			</>
+		</blockquote>
+		<figcaption className="underline underline-offset-1">-{author}</figcaption>
+	</figure>
 );
 
 export default BlockQuote;
