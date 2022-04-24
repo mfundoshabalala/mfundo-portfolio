694402b82146cb3348bf2e97157091441d1b12e7
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-24 22:52:10 +0200

    project page section update

--- components/pages/ProjectsSection.tsx
+++ components/pages/ProjectsSection.tsx
@@ -22,12 +22,12 @@ const ProjectsSection: React.FunctionComponent = () => {
 
 	return (
 		<PageSection pageID="projects">
-			<h1 className="w-full text-center text-5xl font-black uppercase tracking-wide py-4">
-				<div className="bg-clip-text bg-gradient-to-r from-orange-800 via-orange-500  to-orange-800">
+			<h1 className="w-full py-10 text-5xl font-black tracking-wide text-center uppercase">
+				<div className="bg-clip-text bg-gradient-to-r from-orange-800 via-orange-500 to-orange-800">
 					<span className="text-transparent">My Work</span>
 				</div>
 			</h1>
-			<div className="flex flex-wrap gap-4 mx-auto mb-12">
+			<div className="flex flex-wrap justify-center gap-4 mx-auto mb-12">
 				{_.map(projects, (project: GithubRepository) => (
 					<ProjectListItem key={project.id} project={project} />
 				))}
