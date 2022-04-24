a4ffa9913b0b9946a7560e5f067ef47d30963e88
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-24 22:42:57 +0200

    project card update

--- components/ProjectListItem.tsx
+++ components/ProjectListItem.tsx
@@ -33,23 +33,25 @@ const ProjectListItem: ProjectListItemProps = ({ project }) => {
 	}, [getGithubRepositoryLanguages]);
 
 	return (
-		<article className="max-w-sm border py-4 rounded-md border-gray-700 shadow-xl hover:shadow min-w-min bg-slate-900 bg-opacity-100 z-30">
+		<article className="z-30 max-w-sm py-4 bg-opacity-100 border border-gray-700 rounded-md shadow-xl hover:shadow min-w-min bg-slate-900">
 			<section className="flex flex-col gap-y-4">
-				<header className="flex justify-between gap-x-4 items-center px-6 h-8 whitespace-nowrap">
+				<header className="flex items-center justify-between h-8 px-6 gap-x-4 whitespace-nowrap">
 					<h1 className="text-xl font-extralight tracking-wide w-[265px] overflow-hidden whitespace-nowrap text-ellipsis">
 						{transformedText}
 					</h1>
-					<div className="flex gap-x-3 items-center">
+					<div className="flex items-center gap-x-3">
 						<IconComponent
 							size="lg"
 							icon={['fab', 'github']}
 							href={project.html_url}
 						/>
-						<IconComponent
-							size="lg"
-							icon={['fas', 'house-user']}
-							href={project.homepage ?? undefined}
-						/>
+						{project.homepage && (
+							<IconComponent
+								size="lg"
+								icon={['fas', 'house-user']}
+								href={`https://${project.homepage}`}
+							/>
+						)}
 					</div>
 				</header>
 				<main className="">
@@ -59,16 +61,16 @@ const ProjectListItem: ProjectListItemProps = ({ project }) => {
 						height={240}
 						alt="..."
 					/>
-					<p className="text-ellipsis text-justify px-6 py-2 font-light leading-5 h-28 overflow-hidden w-96">
+					<p className="px-6 py-2 overflow-hidden font-light leading-5 text-justify text-ellipsis h-28 w-96">
 						{project?.description ?? 'No description available'}
 					</p>
 				</main>
 			</section>
 			<footer className="px-4">
-				<ul className="flex gap-2 flex-wrap mx-auto">
+				<ul className="flex flex-wrap gap-2 mx-auto">
 					{repositoryLanguage.map((language) => (
 						<li key={language}>
-							<span className="inline-block border border-slate-600 px-2 py-1 text-xs rounded-full">
+							<span className="inline-block px-2 py-1 text-xs border rounded-full border-slate-600">
 								{language}
 							</span>
 						</li>
