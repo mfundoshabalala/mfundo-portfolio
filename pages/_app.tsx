7d42b92368ea97a696f49706521d1e7d6b8f47cf
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-24 23:13:37 +0200

    _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from 'next/app';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import '@fortawesome/fontawesome-svg-core/styles.css';
+import 'flowbite';
 // global app styles + tailwindcss default styles
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.css';
 import 'lib/fontawesome';
 // basic site layout component
 import BasicSiteLayout from 'components/layout/BasicSiteLayout';
+
 
 config.autoAddCss = false;
 
