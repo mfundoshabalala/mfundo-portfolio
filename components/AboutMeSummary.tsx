30da332c63258d68a31f1e071d6f7f650949ac06
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-25 10:04:42 +0200

    about page section refactoring

---
+++ components/AboutMeSummary.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const AboutMeSummary: React.FunctionComponent = () => (
+	<div className="flex flex-col justify-center h-full gap-y-4">
+		<h1 className="text-5xl font-black uppercase">About Me</h1>
+		<p className="">
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo debitis id
+			tempora nam architecto sed suscipit, culpa laudantium hic quod. Earum
+			aperiam provident, ipsam culpa hic quia cumque adipisci ab.
+		</p>
+		<p className="">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+			voluptatum, quaerat omnis maxime ut veritatis, ab magnam fugiat facere
+			vero aspernatur autem totam eius cupiditate? Totam omnis quas delectus
+			reiciendis.
+		</p>
+		<p className="">
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo,
+			nesciunt? Optio dolore modi molestias voluptates sed fuga, aspernatur
+			alias inventore quae corporis facere cumque soluta quis cum debitis
+			perspiciatis fugit!
+		</p>
+	</div>
+);
+
+export default AboutMeSummary;
