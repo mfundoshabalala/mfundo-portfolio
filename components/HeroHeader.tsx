2c0c989eedac95c201d6b81c812c590e84cb0c01
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-04-25 11:17:39 +0200

    home page section refactoring

--- components/HeroHeader.tsx
+++ components/HeroHeader.tsx
@@ -1,14 +1,15 @@
-import BlockQuote from './shared/BlockQuote';
-import Subtitle from './shared/Subtitle';
+//
 import Title from './shared/Title';
+import Subtitle from './shared/Subtitle';
+import BlockQuote from './shared/BlockQuote';
 
-const HeroSection: React.FunctionComponent = () => (
+const HeroHeader: React.FunctionComponent = () => (
 	<div className="flex flex-col gap-2 uppercase select-none drop-shadow justify-center -mb-10">
 		<Title />
 		<Subtitle subtitle="I help build user-friendly and accessible websites." />
 		<BlockQuote
-			cite="https://twitter.com/mfundo_shabalala"
 			author="Cory House"
+			cite="https://twitter.com/mfundo_shabalala"
 			className="tracking-tighter border border-slate-600 mt-4 shadow bg-slate-900"
 		>
 			Code is like humor. When you have to explain it, it’s bad.
@@ -16,4 +17,4 @@ const HeroSection: React.FunctionComponent = () => (
 	</div>
 );
 
-export default HeroSection;
+export default HeroHeader;
--- components/pages/HomeSection.tsx
+++ components/pages/HomeSection.tsx
@@ -1,10 +1,10 @@
-import HeroSection from 'components/HeroSection';
+import HeroHeader from 'components/HeroHeader';
 import PageSection from 'components/shared/PageSection';
 import BackgroundPattern from 'components/shared/BackgroundPattern';
 
 const HomeSection: React.FunctionComponent = () => (
 	<PageSection pageID="home" className={['h-screen']}>
-		<HeroSection />
+		<HeroHeader />
 		<BackgroundPattern className="bg-food-pattern" />
 	</PageSection>
 );
