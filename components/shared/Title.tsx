81bf2a34d71cd5cc7cb31ef8bf8ee7cae39ef126
Mfundo Shabalala <mfundoshabalala@gmail.com>
2022-05-13 12:44:11 -0700

    title component update

--- components/shared/Title.tsx
+++ components/shared/Title.tsx
@@ -1,18 +1,10 @@
-import React from 'react';
-
-type TitleProps = React.FunctionComponent<{
-	children?: React.ReactNode[] | React.ReactNode;
-}>;
-
-const Title: TitleProps = ({ children }) => (
-	// const { salutation, name, stream } = children;
-
+const Title: React.FunctionComponent = () => (
 	<>
 		<h1 className="text-xl font-light leading-3 capitalize">Hi I&apos;m</h1>
 		<h2 className="text-6xl font-bold tracking-wide">Mfundo Shabalala</h2>
 		<h3
 			className="self-end w-40 text-2xl font-medium leading-5 tracking-wide
-		text-right text-orange-400 whitespace-pre-wrap flex flex-col"
+        text-right text-orange-400 whitespace-pre-wrap flex flex-col"
 		>
 			<span className="">A frontend</span>
 			<span className="">developer</span>
